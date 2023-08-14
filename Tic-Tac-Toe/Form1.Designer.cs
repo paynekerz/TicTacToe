21554e53d1a3e1ffe4f639c4ecf7c566165a2ea0
Payne Kerz <84155764+paynekerz@users.noreply.github.com>
2023-08-14 01:14:35 -0500

    redoooo

---
+++ Tic-Tac-Toe/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Tic_Tac_Toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.winStreakLabel = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.BackColor = System.Drawing.Color.White;
+            this.A1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.A1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.A1.Location = new System.Drawing.Point(16, 52);
+            this.A1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(100, 92);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = false;
+            this.A1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B2
+            // 
+            this.B2.BackColor = System.Drawing.Color.White;
+            this.B2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B2.Location = new System.Drawing.Point(124, 151);
+            this.B2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(100, 92);
+            this.B2.TabIndex = 1;
+            this.B2.UseVisualStyleBackColor = false;
+            this.B2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B3
+            // 
+            this.B3.BackColor = System.Drawing.Color.White;
+            this.B3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B3.Location = new System.Drawing.Point(232, 151);
+            this.B3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(100, 92);
+            this.B3.TabIndex = 2;
+            this.B3.UseVisualStyleBackColor = false;
+            this.B3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C3
+            // 
+            this.C3.BackColor = System.Drawing.Color.White;
+            this.C3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C3.Location = new System.Drawing.Point(232, 251);
+            this.C3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(100, 92);
+            this.C3.TabIndex = 3;
+            this.C3.UseVisualStyleBackColor = false;
+            this.C3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C1
+            // 
+            this.C1.BackColor = System.Drawing.Color.White;
+            this.C1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C1.Location = new System.Drawing.Point(16, 251);
+            this.C1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(100, 92);
+            this.C1.TabIndex = 4;
+            this.C1.UseVisualStyleBackColor = false;
+            this.C1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // B1
+            // 
+            this.B1.BackColor = System.Drawing.Color.White;
+            this.B1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.B1.Location = new System.Drawing.Point(16, 151);
+            this.B1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(100, 92);
+            this.B1.TabIndex = 5;
+            this.B1.UseVisualStyleBackColor = false;
+            this.B1.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A3
+            // 
+            this.A3.BackColor = System.Drawing.Color.White;
+            this.A3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.A3.Location = new System.Drawing.Point(232, 52);
+            this.A3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(100, 92);
+            this.A3.TabIndex = 6;
+            this.A3.UseVisualStyleBackColor = false;
+            this.A3.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // A2
+            // 
+            this.A2.BackColor = System.Drawing.Color.White;
+            this.A2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.A2.Location = new System.Drawing.Point(124, 52);
+            this.A2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(100, 92);
+            this.A2.TabIndex = 7;
+            this.A2.UseVisualStyleBackColor = false;
+            this.A2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // C2
+            // 
+            this.C2.BackColor = System.Drawing.Color.White;
+            this.C2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.C2.Location = new System.Drawing.Point(124, 251);
+            this.C2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(100, 92);
+            this.C2.TabIndex = 9;
+            this.C2.UseVisualStyleBackColor = false;
+            this.C2.Click += new System.EventHandler(this.buttonClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.White;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(343, 28);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(216, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem1});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(216, 26);
+            this.aboutToolStripMenuItem1.Text = "About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
+            // 
+            // winStreakLabel
+            // 
+            this.winStreakLabel.AutoSize = true;
+            this.winStreakLabel.Location = new System.Drawing.Point(13, 357);
+            this.winStreakLabel.Name = "winStreakLabel";
+            this.winStreakLabel.Size = new System.Drawing.Size(253, 17);
+            this.winStreakLabel.TabIndex = 11;
+            this.winStreakLabel.Text = "X is on a win streak of 50, click to reset";
+            this.winStreakLabel.Click += new System.EventHandler(this.winStreakLabel_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(343, 386);
+            this.Controls.Add(this.winStreakLabel);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.menuStrip1);
+            this.ForeColor = System.Drawing.Color.Maroon;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic-Tac-Toe_in_Csharp";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+        private System.Windows.Forms.Label winStreakLabel;
+    }
+}
+
---
+++ Tic-Tac-Toe/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe
+{
+    public partial class Form1 : Form
+    {
+        
+        
+        
+        //X's turn is represented by true and O's turn is represented by false, with positive values being for X and negative values for O.
+        bool playerTurn = true;
+        int turnCount = 0;
+        int winStreak = 0; 
+
+        public Form1()
+        {
+            InitializeComponent();
+            updateWinStreak("");
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            autoNewGame();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Exiting the application when the "exit" option is clicked in the menu strip.
+            Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            //About Section
+            MessageBox.Show("Tic Tac Toe");
+        }
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            Button theButtton = (Button)sender;
+
+            if (playerTurn)
+            {
+                theButtton.Text = "X";
+                theButtton.Enabled = false;
+            }
+            else
+            {
+                theButtton.Text = "O";
+                theButtton.Enabled = false;
+            }
+            // The maximum number of moves for draws is 9.
+            turnCount++;
+            playerTurn = !playerTurn;
+            checkWinner();
+        }
+
+        private void checkWinner()
+        {
+            /*
+            foreach(Control x in Controls)//emunmeration of all controls{}
+            */
+            bool weHaveWinner = false;
+
+            
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A2.Enabled))
+                weHaveWinner = true;
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B2.Enabled))
+                weHaveWinner = true;
+            else if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C2.Enabled))
+                weHaveWinner = true;
+
+            
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!B1.Enabled))
+                weHaveWinner = true;
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!B2.Enabled))
+                weHaveWinner = true;
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!B3.Enabled))
+                weHaveWinner = true;
+
+            
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!B2.Enabled))
+                weHaveWinner = true;
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!B2.Enabled))
+                weHaveWinner = true;
+              
+
+            if (weHaveWinner)
+            {
+
+                String winner = "";
+
+                if (playerTurn)
+                    winner = "O";
+                else
+                    winner = "X";
+                updateWinStreak(winner);
+
+                MessageBox.Show(winner + " Wins!", "GG");
+                autoNewGame();
+            }
+            else
+            {
+                if(turnCount == 9)
+                {
+                    MessageBox.Show("Draw");
+                    autoNewGame();
+                }
+
+            }
+
+        }
+
+        private void autoNewGame()
+        {
+            playerTurn = true;
+            turnCount = 0;
+
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    // This validation is crucial, as relying solely on exception handling for button processing might lead to premature code exit when encountering different control types, potentially leaving buttons uncleared.
+                    if (c is Button)
+                    {
+                        (c as Button).Enabled = true;
+                        (c as Button).Text = "";
+                    }
+                }
+            }
+            catch { }
+        } 
+
+        /// <summary>
+        /// Updates the label indicating the current win streak
+        /// </summary>
+        /// <param name="winner">Can take "X" or "O" or any other value to reset</param>
+        private void updateWinStreak(string winner)
+        {
+            if (winner == "X")
+            {
+                // If O was on a win streak, zero it before incrementing
+                if (winStreak < 0) 
+                    winStreak = 0;
+                winStreak++;
+            }
+            else if (winner == "O")
+            {
+                // If X was on a win streak, zero it before decrementing
+                if (winStreak > 0) 
+                    winStreak = 0;
+                winStreak--;
+            }
+            else
+            {
+                winStreak = 0;
+            }
+
+            winStreakLabel.Visible = (winStreak != 0);
+            winStreakLabel.Text = String.Format("{0} is on a win streak of {1}", winner, Math.Abs(winStreak));
+        }
+
+        private void winStreakLabel_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to reset the win streak?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                updateWinStreak("");
+            }
+        }
+    }
+}
